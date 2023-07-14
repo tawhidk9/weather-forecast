353710dcc5d2c1af28ecbaf19cd62475de862070
Tawhid <tawhidkamal5@gmail.com>
2023-07-13 21:53:20 -0400

    created dependencies, and created event list for form and history

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,25 @@
-//translate city name into coordinates
+// DEPENDENCIES
+var apiKey = "YOUR_API_KEY";
+var searchFormEl = document.querySelector("#search-form");
+var searchInputEl = document.querySelector("#search-input");
+var searchHistoryEl = document.querySelector("#search-history");
+var currentWeatherEl = document.querySelector("#current-weather");
+var forecastEl = document.querySelector("#forecast");
 
-//
+// Form Submission
+searchFormEl.addEventListener("submit", function (event) {
+  event.preventDefault();
+  var city = searchInputEl.value.trim();
+  if (city) {
+    getWeather(city);
+    searchInputEl.value = "";
+  }
+});
+
+// Search History
+searchHistoryEl.addEventListener("click", function (event) {
+  var city = event.target.getAttribute("data-city");
+  if (city) {
+    getWeather(city);
+  }
+});
