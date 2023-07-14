4cdb624e652c464508acb38d5fc827ca3039a9e7
Tawhid <tawhidkamal5@gmail.com>
2023-07-13 22:02:49 -0400

    created weather display

--- assets/script.js
+++ assets/script.js
@@ -24,7 +24,49 @@ searchHistoryEl.addEventListener("click", function (event) {
   }
 });
 
+// Get weather data 
 function getWeather(city) {
     var currentWeatherUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
     var forecastUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + apiKey;
+}
+
+//Fetch current weather
+
+fetch(currentWeatherUrl)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function(data) {
+        displayCurrentWeather(data, city);
+    });
+
+
+//Fetch forecast
+
+fetch(forecastUrl)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function(data) {
+        displayForecast(data);
+    });
+
+// Display Current Weather 
+function displayCurrentWeather(data, city) {
+    currentWeatherEl.innerHTML = "";
+
+    var cityNameEl = document.createElement("h2");
+    cityNameEl.textContent = city + " (" + new Date().toLocaleDateString() + ")";
+    
+    var weatherIconEl = document.createElement("img");
+    weatherIconEl.setAttribute("src", "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png");
+    
+    var temperatureEl = document.createElement("p");
+    temperatureEl.textContent = "Temperature: " + data.main.temp + " °C";
+    
+    var humidityEl = document.createElement("p");
+     humidityEl.textContent = "Humidity: " + data.main.humidity + "%";
+    
+     var windSpeedEl = document.createElement("p");
+    windSpeedEl.textContent = "Wind Speed: " + data.wind.speed + " m/s";
 }
